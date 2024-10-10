47d41a6b594652da06888aadaaa69749a94958ba
thomas <758641+cavecafe@users.noreply.github.com>
2024-10-09 20:05:56 -0400

    brew

--- brew/setup-brew.sh
+++ brew/setup-brew.sh
@@ -7,8 +7,9 @@ PROJECT_DESC="LOAR - Local Archive Utility"
 PROJECT_HOME="https://github.com/cavecafe-cc/loar"
 VERSION_VALUE='0.7.0'
 PROJECT_NAME_LOWER=$(echo "$PROJECT_NAME" | tr '[:upper:]' '[:lower:]')
-PROJECT_TAR_URL=getLatestFileUrl "loar_osx-arm64.tar.gz"
-PROJECT_TAR_SHA256_URL=getLatestFileUrl "loar_osx-arm64.tar.gz.SHA256"
+
+
+# Get the latest release URL
 
 function getLatestFileUrl() {
   local ASSET_NAME=$1
@@ -17,10 +18,9 @@ function getLatestFileUrl() {
     "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | \
     jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
   echo "$LATEST_RELEASE_URL"
-  return "$LATEST_RELEASE_URL"
 }
 
-# for private repo
+## for private repo
 #GITHUB_PAT=${GITHUB_PAT_PULL_BINARY_LOAR:-"your_github_token_here"}
 #function getLatestFileUrl() {
 #  local ASSET_NAME=$1
@@ -29,11 +29,15 @@ function getLatestFileUrl() {
 #    "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/releases/latest" | \
 #    jq -r ".assets[] | select(.name == \"$ASSET_NAME\") | .browser_download_url")
 #  echo "$LATEST_RELEASE_URL"
-#  return "$LATEST_RELEASE_URL"
 #}
+## actual files are like the following (for test)
 #PROJECT_TAR=$(curl -sL -H "Authorization: token $GITHUB_PAT" "$PROJECT_TAR_URL")
 #PROJECT_TAR_SHA256=$(curl -sL -H "Authorization: token $GITHUB_PAT" "$PROJECT_TAR_SHA256_URL")
 
+# Get the latest release
+PROJECT_TAR_URL=$(getLatestFileUrl "loar_osx-arm64.tar.gz")
+PROJECT_TAR_SHA256_URL=$(getLatestFileUrl "loar_osx-arm64.tar.gz.SHA256")
+PROJECT_TAR_SHA256=$(curl -sL "$PROJECT_TAR_SHA256_URL")
 
 # Create Brew Formula
 cat <<EOF > "$PROJECT_NAME_LOWER".rb
